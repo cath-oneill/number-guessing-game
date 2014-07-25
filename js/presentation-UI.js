f7f1ba27a7650f37965e129a29c23e56e52370b3
Catherine O'Neill <cathzozo@gmail.com>
2014-07-25 15:16:11 -0500

    mostly working UI, except feedback text

---
+++ js/presentation-UI.js
@@ -0,0 +1,40 @@
+(function() {
+	var Presentation = function() {
+		var secretNumber;
+		var $form = $('form');
+  		var $feedback_text = $('#feedback_text');
+  		var $feedback = $('#feedback');
+  		var $guess = $('#guess');
+
+		var feedbackMessage = function(currentGuess) {
+			var message = logic.checkGuessVsSecret(currentGuess, secretNumber);
+			if (message === "successMessage") {
+				return "Congrats! You got the correct number!";
+			} else if (message === "tooHighMessage") {
+				return "Your guess is too high! Guess again.";
+			} else if (message === "tooLowMessage") {
+				return "Your guess is too low! Guess again.";
+			} else if (message === "incorrectEntry") {
+				return "Try again. Your entry was invalid. Please enter a number between 1 and 100.";
+			};
+		};
+
+		$('#submit-button').on('click', function(e) {	
+			var currentGuess = +$('#guess').val();
+			$('#feedback_text').text(feedbackMessage(currentGuess));
+		    // $feedback.show();
+		    // $guess.val('')
+		});
+
+
+
+		$('#start-button').on('click', function() {	
+		    secretNumber = logic.setSecretNumber();
+		    console.log("Secret number: " + secretNumber);
+		    // $form.show();
+		    // $feedback.hide();
+		});	
+	};//close Presentation
+	window.presentation = Presentation;
+	presentation();
+})();
